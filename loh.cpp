6b53669b48db57c586c5016354b60cd81a40a8e6
DalInar <oryx@umich.edu>
2014-10-02 16:05:48 -0400

    Now with MPI and cmake!

--- loh.cpp
+++ loh.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <mpi.h>
 
-int main() {
-  std::cout<<"Hello Worlds!"<<std::endl;
-  std::cout<<"Here are some changes!"<<std::endl;
+int main(int argc, char *argv[]) {
+	int rank, size;
+	MPI_Init(&argc, &argv); //Start up MPI
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank); //Get current processor number
+	MPI_Comm_size(MPI_COMM_WORLD, &size); //Get number of processors in communicator
+	std::cout<<"Hello worlds from process "<<rank<<" of "<<size<<std::endl;
+	MPI_Finalize();
+
   return 0;
 }
